d4d32b471c869b7b2e4337c6aafb8cca3e3af7b0
fangsheng <445317262@qq.com>
2020-11-19 23:47:39 +0800

    1

--- src/com/ss/volatileLearn.java
+++ src/com/ss/volatileLearn.java
@@ -1,4 +1,11 @@
 package com.ss;
 
 public class volatileLearn {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            new Thread(() -> {
+
+            }, String.valueOf(i)).start();
+        }
+    }
 }
